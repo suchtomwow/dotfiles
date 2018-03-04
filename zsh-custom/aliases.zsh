d86b4f38bd16dc1bcd2699eea045a24b561cd83e
Thomas Carey <jthomascarey@gmail.com>
2018-03-03 21:51:13 -0700

    Using Neovim for now

--- zsh-custom/aliases.zsh
+++ zsh-custom/aliases.zsh
@@ -2,6 +2,7 @@ alias ls='ls -aGFh'
 alias ping='ping -c 4'
 alias ddd='fastlane run clear_derived_data'
 alias rm=trash
+alias vim=nvim
 
 function git-prune-local {
     git fetch --prune && git branch -r | awk '{print $1}' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print $1}' | xargs git branch -D
