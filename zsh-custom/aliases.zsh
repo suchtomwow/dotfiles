0770295954101fc9339f753f94512a137366c084
Thomas Carey <jthomascarey@gmail.com>
2019-04-15 11:17:19 -0600

    Add bundle exec alias

--- zsh-custom/aliases.zsh
+++ zsh-custom/aliases.zsh
@@ -3,6 +3,7 @@ alias ping='ping -c 4'
 alias ddd='fastlane run clear_derived_data'
 alias rm=trash
 alias vim=nvim
+alias b='bundle exec'
 
 function git-prune-local {
     git fetch --prune && git branch -r | awk '{print $1}' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print $1}' | xargs git branch -D
