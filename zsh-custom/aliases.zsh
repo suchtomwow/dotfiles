0506245420d0cce10f1c17124f7c9fbfd19e9cbe
Thomas Carey <jthomascarey@gmail.com>
2019-06-14 08:48:35 -0600

    more good stuff probably

--- zsh-custom/aliases.zsh
+++ zsh-custom/aliases.zsh
@@ -8,3 +8,7 @@ alias b='bundle exec'
 function git-prune-local {
     git fetch --prune && git branch -r | awk '{print $1}' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print $1}' | xargs git branch -D
 }
+
+export NVM_DIR="$HOME/.nvm"
+  [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"  # This loads nvm bash_completion
